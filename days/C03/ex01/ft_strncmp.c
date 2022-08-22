eabacf8278d9c0e820f9cad790c36668be164cc7
Moadtell <moadtell@gmail.com>
2022-08-22 16:17:11 +0100

    up C03

---
+++ days/C03/ex01/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtellami <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 19:54:47 by mtellami          #+#    #+#             */
+/*   Updated: 2022/07/26 15:07:30 by mtellami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]) && i < n)
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char	s1[] = " ";
+	char	s2[] = "f";
+	char	s = ft_strncmp(s1, s2, 1);
+	printf("%d", ft_strncmp(s1, s2, 1));
+	printf("%c", s);
+}
